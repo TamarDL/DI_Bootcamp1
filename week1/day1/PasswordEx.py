1d9cd623743822c13b98ca910c081ad684163b2d
TamarDL <tamardessel@gmail.com>
2024-08-16 23:05:59 +0300

    FINISH

--- week1/day1/PasswordEx.py
+++ week1/day1/PasswordEx.py
@@ -2,4 +2,4 @@ username = input('what is your username?')
 password = input('enter your password')
 password_length = len(password)
 hidden_password = "*" * password_length
-print (f'{username },your password: {password},have {len(password)} letters long')
+print (f'{username },your password {hidden_password},have {password_length} letters long')
