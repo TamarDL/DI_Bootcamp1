c9c8dbba1885b73265ffdc695648e20a2da033f2
TamarDL <tamardessel@gmail.com>
2024-08-17 17:05:17 +0300

    ex5

---
+++ week1/day2/ex5.py
@@ -0,0 +1,5 @@
+#Use a for loop to print all numbers from 1 to 20, inclusive.
+#Using a for loop, that loops from 1 to 20(inclusive), print out every element which has an even index.
+numbers = range(1, 21)
+for number in numbers:
+  print(number)
