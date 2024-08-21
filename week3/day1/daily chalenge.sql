c34ae110220f1817b6e101a83b38f357cafb8c28
TamarDL <tamardessel@gmail.com>
2024-08-21 15:36:18 +0300

    daily chalenge day 1

---
+++ week3/day1/daily chalenge.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actors;
+--select COUNT (actor_id) from actors;
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+--  VALUES
+--  ('', '','09/06/1963',7)
+--select COUNT (actor_id) from actors; 
+--The outcome will be a another row extra to the account.
