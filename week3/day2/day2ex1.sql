c60c5b6b9fd8413a5764fc18c88c5d25cf5a5750
TamarDL <tamardessel@gmail.com>
2024-08-24 15:32:20 +0300

    week3

---
+++ week3/day2/day2ex1.sql
@@ -0,0 +1,12 @@
+--select * from customer;
+--select (first_name , last_name) AS full_name from customer;
+--select create_date from customer;
+--select * from customer order by first_name DESC;
+--select film_id, title, description, release_year, rental_rate from film order by rental_rate asc;
+--select address, phone from address WHERE district in ('Texas');
+--select * from film where film_id = 15 or film_id=150;
+--select film_id, title, description,length, rental_rate from film where (title='The lion king');
+--select film_id, title, description,length, rental_rate from film where (title like 'Th%');
+--select * from film order by rental_rate asc limit 10;
+
+
